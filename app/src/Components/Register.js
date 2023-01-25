a9dc92b3069008dc6984cbaf1c35aec7dbcd15a5
VitorNascDev <vitornascimentobatista1@gmail.com>
2023-01-24 23:42:52 -0300

    Adding the perfil field on the register component.

--- app/src/Components/Register.js
+++ app/src/Components/Register.js
@@ -5,16 +5,16 @@ export default function Register(props) {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
     const [email, setEmail] = useState('')
-    const [Perfis, setPerfis] = useState([])
+    const [Perfis, setPerfis] = useState([{position: 1, text: ""}])
 
-    function onChange(e) {
+    function OnChange(e) {
         const changFunc = {
-            username: () => { setUsername(e.target.value)},
-            password: () => { setPassword(e.target.value)},
-            email: () => { setEmail(e.target.value)}
+            UsernameRegisterInput: () => { setUsername(e.target.value)},
+            PasswordRegisterInput: () => { setPassword(e.target.value)},
+            EmailRegisterInput: () => { setEmail(e.target.value)}
         }
 
-        console.log(e)
+        changFunc[e.target.id]()
     }
 
     function CloseModal(e) {
@@ -23,15 +23,88 @@ export default function Register(props) {
         }
     }
 
+    function RegisterFunction(e) {
+        e.preventDefault()
+        console.log(`Username: ${username}`)
+        console.log(`Password: ${password}`)
+        console.log(`Email: ${email}`)
+        console.log(Perfis)
+    }
+
+    function AddRemovePerfil(e) {           // The function to add or remove a perfil field
+        e.preventDefault()
+        console.log(e.target.id)
+
+        if (e.target.id === "AddPerfil") {  // When the Button to add the perfil is clicked
+            const newPerfil = {
+                position: Perfis[Perfis.length - 1].position + 1,
+                text: ''
+            }
+
+            if (newPerfil.position !== 6) {  // The condition to not over pass the maximum num of perfis
+                setPerfis([...Perfis, newPerfil])
+            } else {
+                alert('Maximum 5 Perfis')
+            }
+        }
+
+        if (e.target.id === "RemovePerfil") {
+            if (Perfis.length - 1 !== 0) {
+                setPerfis(Perfis.filter(data => data.position !== Perfis.length))
+            } else {                        // The condition to not remove all the profiles
+                alert('Can\'t Remove all perfis')
+            }
+        }
+    }
+
+    function OnChangePerfilInput(e) {
+        const id = e.target.id
+        const num = Number(id.slice(id.length - 1, id.length))
+
+        setPerfis(Perfis.filter(data => {
+            if (data.position === num) {
+                data.text = e.target.value
+            }
+
+            return data
+        }))
+    }
+
     return (
         <div className={props.registerModal ? "Modal" : "Hidden"} onClick={CloseModal}>
             <form className="LoginFormContainer">
                 <h2>Register</h2>
-                <input type="username" id="username" onChange={onChange} placeholder="Type your username"/>
-                <input type="password" id="password" onChange={onChange} placeholder="Type your password"/>
-                <input type="email" id="email" onChange={onChange} placeholder="Type your email" />
+                <input type="username" id="UsernameRegisterInput" onChange={OnChange} placeholder="Type your username"/>
+                <input type="password" id="PasswordRegisterInput" onChange={OnChange} placeholder="Type your password"/>
+                <input type="email" id="EmailRegisterInput" onChange={OnChange} placeholder="Type your email" />
 
-                <button>Register</button>
+                <div className="RegisterPerfilContainer">
+                    <h2>Perfil</h2>
+
+                    <div className="PerfilInputContainer">
+
+                        {Perfis.map(data => {       // Show all the inputs of the perfil
+                            return (
+                                <input
+                                    type="text"
+                                    key={data.position}
+                                    value={data.text}
+                                    placeholder={`Write the ${data.position} Perfil...`}
+                                    id={`PerfilInput${data.position}`}
+                                    onChange={OnChangePerfilInput}
+                                />
+                            )
+                        })}
+
+                    </div>
+
+                    <div onClick={AddRemovePerfil} className="RegisterPerfilButtonContainer">
+                        <button id="RemovePerfil">Remove one perfil</button>
+                        <button id="AddPerfil">Add one perfil</button>
+                    </div>
+                </div>
+
+                <button onClick={RegisterFunction}>Register</button>
             </form>
         </div>
     )
