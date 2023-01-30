4cd719a592eeac25679eeb17c4c2d3cd593cdcf0
VitorNascDev <vitornascimentobatista1@gmail.com>
2023-01-30 19:37:40 -0300

    Adding react-router-dom. The route "/test" is for testing purposes

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,14 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { 
+    BrowserRouter as Router,
+    Routes,
+    Route
+} from 'react-router-dom'
 
 import Header from './Components/Header'
+import ListFiles from './Components/ListFiles'
 import Login from './Components/Login'
+import Perfil from './Components/Perfil'
 import Register from './Components/Register'
 import './style.css'
 
@@ -9,24 +16,49 @@ export default function App() {
 
     const [loginModal, setLoginModal] = useState(false)
     const [registerModal, setRegisterModal] = useState(false)
+    const [token, setToken] = useState('')
+
+    useEffect(() => {
+        console.log('Hello World')
+
+        if (token === '') {
+            console.log('Token does\'t exist')
+        } else {
+            console.log('Setting up the token to the hook')
+        }
+    }, [])
 
     return (
         <div>
-            <Header 
-                loginModal={loginModal}
-                setLoginModal={setLoginModal}
-                registerModal={registerModal}
-                setRegisterModal={setRegisterModal}
-            />
+            <Router>
 
-            <Login 
-                loginModal={loginModal}
-                setLoginModal={setLoginModal}
-            />
-            <Register 
-                registerModal={registerModal}
-                setRegisterModal={setRegisterModal}
-            />
+                    <Header 
+                        loginModal={loginModal}
+                        setLoginModal={setLoginModal}
+                        registerModal={registerModal}
+                        setRegisterModal={setRegisterModal}
+                    />
+
+                    <Login 
+                        loginModal={loginModal}
+                        setLoginModal={setLoginModal}
+                    />
+
+                    <Register 
+                        registerModal={registerModal}
+                        setRegisterModal={setRegisterModal}
+                    />
+
+                <Routes>
+                    <Route path="/ListAllVideos" element={ <ListFiles/> }/>
+                    <Route path="/test" element={<Perfil />} />
+                </Routes>
+            </Router>
+            
+
+            
+            
+
         </div>
     )
 }
