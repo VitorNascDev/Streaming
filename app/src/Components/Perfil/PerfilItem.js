ba93f7f1adf7a68f726cb262d74a7799763cbcff
VitorNascDev <vitornascimentobatista1@gmail.com>
2023-01-31 17:34:41 -0300

    Creating the perfil page.

---
+++ app/src/Components/Perfil/PerfilItem.js
@@ -0,0 +1,15 @@
+import PerfilPhoto from '../../Assets/'
+
+export default function PerfilItem() {
+    return (
+        <li>
+            <a href="">
+            
+                <img src={PerfilPhoto} />
+
+                <h2>Vitor</h2>
+            
+            </a>
+        </li>
+    )
+}
---
+++ app/src/Components/Perfil/index.js
@@ -0,0 +1,11 @@
+import PerfilItem from "./PerfilItem";
+
+export default function Perfil() {
+    return (
+        <div>
+            <ul>
+                <PerfilItem />
+            </ul>
+        </div>
+    )
+}
