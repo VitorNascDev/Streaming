63d2d00851c240badcd4cc5bdac3cdb33f0a3fb7
VitorNascDev <vitornascimentobatista1@gmail.com>
2023-01-31 17:28:08 -0300

    Returning a Acess Denied if the user don`t have a token, when list all the files

--- app/src/Components/ListFiles/index.js
+++ app/src/Components/ListFiles/index.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import VideoItem from './VideoItem'
 
-export default function ListFiles() {
+export default function ListFiles(props) {
 
     const [videos, setVideo] = useState([])
 
@@ -9,7 +9,8 @@ export default function ListFiles() {
         fetch('/getAllFiles', {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'UserToken': props.token.token
             }
         })
         .then(response => response.json())
@@ -17,13 +18,24 @@ export default function ListFiles() {
 
     }, [])
 
-    return (
-        <ul className='VideoItemContainer'>
-            {videos.map(video => {
-                return (
-                    <VideoItem data={video} key={`${video}${Math.floor(Math.random() * 100)}`}/>
-                )
-            })}
-        </ul>
-    )
+    if (props.token.tokenExist) {
+
+        return (
+            <ul className='VideoItemContainer'>
+                {videos.map(video => {
+                    return (
+                        <VideoItem token={props.token} data={video} key={`${video}${Math.floor(Math.random() * 100)}`}/>
+                    )
+                })}
+            </ul>
+        )
+    } else {
+
+        return (
+            <div>
+                <h1>Acess Denied</h1>
+            </div>
+        )
+    }
+
 }
