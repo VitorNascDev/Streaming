bf19ebf0249cf7da15ad492972a597ee3aae8c78
VitorNascDev <vitornascimentobatista1@gmail.com>
2023-01-31 17:41:20 -0300

    Changing the test token to the user token.

--- app/src/Components/ListFiles/VideoItem.js
+++ app/src/Components/ListFiles/VideoItem.js
@@ -1,11 +1,10 @@
 export default function VideoItem(props) {
-
-    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkxvcmVtIElwc3VtIiwiaWF0IjoxNjc1MTExODQxfQ.3LIalTTvljjjxVqV78_Rcet4SAAnbvCid1AJiUQL-fI"
+    
 
     return (
         <li className="VideoItem">
-            <a href="#">
-                <img src={`/video/${token}/${props.data}.jpg`}/>
+            <a href={`/test/${props.data}/`}>
+                <img src={`/video/${props.token.token}/${props.data}.jpg`}/>
 
                 <h4>{props.data}</h4>
             </a>
