00ab55e8f635c4dfcc8cc4f8448a3ca76805d00e
VitorNascDev <vitornascimentobatista1@gmail.com>
2023-02-08 20:38:04 -0300

    Creating a route to change the category of a video

--- Router/Router.js
+++ Router/Router.js
@@ -1,12 +1,23 @@
 const Router = require('express').Router()
+const category = require('../Controller/Category')
 const Auth = require('../Controller/Auth')
 const Data = require('../Controller/Data')
 
 
+// Auth Requests
 Router.post('/Auth/Login', Auth.Login)          // The login Route
 Router.post('/Auth/Register', Auth.Register)    // The Register Route
 
+// User Requests
+
+Router.post('/Auth/getProfiles', Auth.getProfiles)
+
+
+// Get Video Requests
 Router.get('/video/:UserToken/:data', Auth.ProtectTheFiles)   // The Route to get a data
 Router.post('/getAllFiles', Data.getAllVideos)     // The route to get a array with all the files inside de video folder
+
+// Category Requests
+Router.post('/addNewCategory/', category.addNewCategory)
 
 module.exports = Router
