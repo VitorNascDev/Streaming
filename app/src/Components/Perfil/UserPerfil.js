94d7bf98cda13d533b86a663a088a6d00a43c425
VitorNascDev <vitornascimentobatista1@gmail.com>
2023-02-08 23:45:33 -0300

    React Component: User Profile Component.

---
+++ app/src/Components/Perfil/UserPerfil.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import ListFiles from "../ListFiles"
+
+export default function UserPerfil(props) {
+
+    const [videos, setVideo] = useState({})
+    const { perfil } = useParams()
+
+    useEffect(() => {
+
+
+        fetch('/getAllFiles', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'UserToken': props.token.token
+            }
+        })
+        .then(response => response.json())
+        .then(data => setVideo(data))
+
+    }, [])
+
+    return (
+        <div>
+            <h1>{perfil}</h1>
+
+            <ListFiles token={props.token} Videos={videos} />
+        </div>
+    )
+}
