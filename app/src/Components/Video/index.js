86d98c2f25858715e672820ce84c0f27e39486a6
VitorNascDev <vitornascimentobatista1@gmail.com>
2023-02-10 23:57:32 -0300

    React Component: Showing the category of the video

--- app/src/Components/Video/index.js
+++ app/src/Components/Video/index.js
@@ -1,10 +1,30 @@
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 
 export default function Video(props) {
 
     // The video component that open the video
 
+    const [categorys, setCategorys] = useState([])
     const { video } = useParams()
+
+    useEffect(() => {
+
+        // This useEffect is to get the category list
+
+
+        fetch('/getCategoryList', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ Title: video })
+        })
+        .then(response => response.json())
+        .then(data => setCategorys([...data]))
+
+
+    }, [])
 
     // Returning a acess denied message in case the user doesn't have the token
 
@@ -15,6 +35,28 @@ export default function Video(props) {
                     <source src={`/video/${props.token.token}/${video}.mp4`}></source>
                 </video>
                 <h1>{video}</h1>
+
+                <div className="CategorysContainer">
+
+                    
+                    {/* // Here we gonna check if the video has any category */}
+
+                    {categorys.length ? (
+                        <ul>
+                            {categorys.map(data => {
+                                return (
+                                    <li>{data}</li>
+                                )
+                            })}
+                        </ul>
+                    ) : 
+                    (
+                        <h1>Not Exist</h1>
+                    )}
+
+                    
+
+                </div>
             </div>
         )
     } else {
